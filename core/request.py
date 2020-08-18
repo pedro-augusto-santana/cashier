158e58c03ee136030dc514e776b564fa64192e61
Pedro Augusto <pedroaugustoansantana@gmail.com>
2020-08-17 21:40:31 -0300

    Dates are now parsed with the datetime module

--- core/request.py
+++ core/request.py
@@ -1,5 +1,7 @@
 # request.py
 
+from datetime import datetime
+
 from urllib3 import PoolManager
 
 def execute_request(opts: dict)->bytes:
@@ -23,17 +25,19 @@ def execute_request(opts: dict)->bytes:
         queries.append(base)
     
     if "period" in opts.keys():
-        period = f"start_at={'-'.join(opts['period'][0:3])}&end_at={'-'.join(opts['period'][3:])}"
+        start_date = datetime(*map(int, opts['period'][0:3])).date()
+        end_date = datetime(*map(int, opts['period'][3:])).date()
+        period = f"start_at={start_date}&end_at={end_date}"
         queries.insert(0, period)
         query_str = "history?" + "&".join(queries)
 
     elif "date" in opts.keys():
-        date = "-".join(opts["date"])
-        queries.insert(0, date)
-        query_str = queries[0] + "?" + "&".join(queries[1:])
+        date = str(datetime(*map(int, opts["date"])).date())
+        query_str = date + "?" + "&".join(queries[1:])
     
     else:
-        query_str = "latest?" + "&".join(queries)
+        today = datetime.today().date()
+        query_str = str(today) + "?" + "&".join(queries)
 
     data = http.request("GET",base_url+query_str).data
     return data
