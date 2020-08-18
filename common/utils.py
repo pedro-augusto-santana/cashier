8208554cb84fc1468918dd2798c15788514bec58
Pedro Augusto <pedroaugustoansantana@gmail.com>
2020-08-17 22:07:56 -0300

    Minor changes in utils.py ( added info display function )

--- common/utils.py
+++ common/utils.py
@@ -3,5 +3,8 @@
 # TODO
 # - ADD CUSTOM ERROR PRINTS
 
-def error(msg:str)-> None:
-    print(f"\033[1;31m{msg}\033[1;31;40m")
+def error (msg: str)-> None:
+    print(f"\033[1;31m{msg}\033[0m")
+
+def info (msg: str)-> None:
+    print(f"\033[1m{msg}\033[0m")
