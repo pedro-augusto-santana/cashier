d550cf19ab7babd944299854c0d570c20d70ec05
Pedro Augusto <pedroaugustoansantana@gmail.com>
2020-08-19 22:04:35 -0300

    Period results are now ordered by date

--- core/cli.py
+++ core/cli.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 from cashier.common.utils import error, info
 
@@ -10,25 +11,25 @@ def display(data:bytes)-> None:
     content: dict = json.loads(data.decode("utf-8"))
 
     if "date" in content.keys():
-        info(f"Base currency: {content['base']}")
+        info(f"Base currency: {content['base']}\n")
         print("Currency(ies) requested")
 
         for currency, rate in content["rates"].items():
             print("\t" + str(currency) + "\t" + str(rate))
 
-        info(f"Date of search: {content['date']}")
+        info(f"\nDate of search: {content['date']}")
     
     elif "start_at" in content.keys():
-        info(f"Base currency: {content['base']}")
+        info(f"Base currency: {content['base']}\n")
         
         rates = list(content.pop("rates").items())
-        
+        rates.sort(key=str) # sorting is using alphanumeric sorting, works well enough
         for date, exchs in rates:
             info(f"Rates on date: {date}")
             for exch in exchs.items():
                 print(f"\t{exch[0]}\t{exch[1]}")
 
-        info(f"Start date:  {content['start_at']}")
+        info(f"\nStart date:  {content['start_at']}")
         info(f"End date:    {content['end_at']}")
 
     elif "error" in content.keys():
